0e8869a5a2d5d7872d2424bf9eeb0a5b58e9f6db
gully <igully@gmail.com>
2016-04-05 17:59:11 +0800

    Add argparser for number of samples and incremental progress, force -np.inf for stellar params out of grid, add multithreading support, quiet mode but with time stamp for progress bar.

--- scripts/star_so.py
+++ scripts/star_so.py
@@ -2,23 +2,20 @@
 
 # All of the argument parsing is done in the `parallel.py` module.
 
+import time
 import numpy as np
 import Starfish
 from Starfish.model import ThetaParam, PhiParam
 
-
-#import argparse
-#parser = argparse.ArgumentParser(prog="star_so.py", description="Run Starfish fitting model in single order mode with many walkers.")
-#parser.add_argument("--sample", choices=["ThetaCheb", "ThetaPhi", "ThetaPhiLines"], help="Sample the all stellar and nuisance parameters at the same time.")
-#parser.add_argument("--samples", type=int, default=5, help="How many samples to run?")
-#parser.add_argument("--incremental_save", type=int, default=0, help="How often to save incremental progress of MCMC samples.")
-#parser.add_argument("--use_cov", action="store_true", help="Use the local optimal jump matrix if present.")
-#args = parser.parse_args()
+import argparse
+parser = argparse.ArgumentParser(prog="star_so.py", description="Run Starfish fitting model in single order mode with many walkers.")
+parser.add_argument("--samples", type=int, default=5, help="How many samples to run?")
+parser.add_argument("--incremental_save", type=int, default=100, help="How often to save incremental progress of MCMC samples.")
+args = parser.parse_args()
 
 import os
 
 import Starfish.grid_tools
-from Starfish.samplers import StateSampler
 from Starfish.spectrum import DataSpectrum, Mask, ChebyshevSpectrum
 from Starfish.emulator import Emulator
 import Starfish.constants as C
@@ -34,7 +31,7 @@ import gc
 import logging
 
 from itertools import chain
-from collections import deque
+#from collections import deque
 from operator import itemgetter
 import yaml
 import shutil
@@ -114,7 +111,7 @@ class Order:
             phi = PhiParam.load(fname)
             self.chebyshevSpectrum.update(phi.cheb)
 
-        self.resid_deque = deque(maxlen=500) #Deque that stores the last residual spectra, for averaging
+        #self.resid_deque = deque(maxlen=500) #Deque that stores the last residual spectra, for averaging
         self.counter = 0
 
         self.emulator = Emulator.open()
@@ -209,7 +206,7 @@ class Order:
         fix_logg = Starfish.config.get("fix_logg", None)
         if fix_logg is not None:
             p.grid[1] = fix_logg
-        print("grid pars are", p.grid)
+        #print("grid pars are", p.grid)
 
         self.logger.debug("Updating Theta parameters to {}".format(p))
 
@@ -361,13 +358,17 @@ model = SampleThetaPhi(debug=True)
 model.initialize((0,0))
 
 def lnprob_all(p):
-    pars1 = ThetaParam(grid=p[0:3], vz=p[3], vsini=p[4], logOmega=p[5])
-    model.update_Theta(pars1)
-    # hard code npoly=3 (for fixc0 = True with npoly=4) !
-    pars2 = PhiParam(0, 0, True, p[6:9], p[9], p[10], p[11])
-    model.update_Phi(pars2)
-    lnp = model.evaluate()
-    return lnp
+    try:
+        pars1 = ThetaParam(grid=p[0:3], vz=p[3], vsini=p[4], logOmega=p[5])
+        model.update_Theta(pars1)
+        # hard code npoly=3 (for fixc0 = True with npoly=4) !
+        pars2 = PhiParam(0, 0, True, p[6:9], p[9], p[10], p[11])
+        model.update_Phi(pars2)
+        lnp = model.evaluate()
+        return lnp
+    except C.ModelError:
+            self.logger.debug("ModelError in stellar parameters, sending back -np.inf {}".format(p))
+            return -np.inf
 
 import emcee
 
@@ -385,10 +386,20 @@ p0_std = [5, 0.02, 0.02, 0.5, 0.5, -0.01, -0.005, -0.005, -0.005, 0.01, 0.001, 0
 
 p0_ball = emcee.utils.sample_ball(p0, p0_std, size=nwalkers)
 
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob_all)
+sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob_all, threads=8)
 
 #sampler.lnprobfn.f(p0)
-sampler.run_mcmc(p0_ball, 2)
+
+
+
+nsteps = args.samples
+ninc = args.incremental_save
+for i, (pos, lnp, state) in enumerate(sampler.sample(p0_ball, iterations=nsteps)):
+    if (i+1) % ninc == 0:
+        time.ctime() 
+        t_out = time.strftime('%Y %b %d,%l:%M %p') 
+        print("{0}: {1:}/{2:} = {3:.1f}%".format(t_out, i, nsteps, 100 * float(i) / nsteps))
+        np.save('temp_emcee_chain.npy',sampler.chain)
 
 np.save('emcee_chain.npy',sampler.chain)
 
