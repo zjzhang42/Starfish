6a0c12323b3872ec6bd651a19955ad75ee89f39d
zjzhang42 <zhoujian@hawaii.edu>
2018-12-12 17:29:51 -1000

    add a new GridInterface for Marley18 {M0, M+0.5} models

--- Starfish/grid_tools.py
+++ Starfish/grid_tools.py
@@ -6,6 +6,9 @@
 # ZJ Zhang (Jul 20th, 2017)   (REVISE --- "class SPEX_PRZ(Instrument)")
 # ZJ Zhang (Jan 27th, 2018)   (REVISE --- combine "MarleyInterface" and "MarleyMay2017Interface" to the single one)
 # ZJ Zhang (Aug 15th, 2018)   (ADD --- "MarleyMp0d5GridInterface"
+# ZJ Zhang (Dec 12th, 2018)   (REVISE --- change the self.name from Marley to MarleyMp0d5 in "MarleyMp0d5GridInterface")
+# ZJ Zhang (Dec 12th, 2018)   (REVISE --- skiprows=2 while loading the models in "MarleyMp0d5GridInterface")
+# ZJ Zhang (Dec 12th, 2018)   (ADD --- "MarleygridM0Mp0d5GridInterface")
 #
 #################################################
 
@@ -180,8 +183,8 @@ class RawGridInterface:
 
 
 class MarleyGridInterface(RawGridInterface):
-    ''' An interface to the 2017 Marley and collaborators synthetic spectral library
-        - added by Michael Gully and modified by ZJ Zhang
+    ''' An interface to the Sonoara atmospheric models by Marley et al. (2018 in prep)
+        - originally added by Michael Gully and modified by ZJ Zhang
         '''
     def __init__(self, air=False, norm=False, wl_range=[4000, 50000], base=os.path.expandvars(Starfish.grid["raw_path"])):
         ## basic information of model grids
@@ -201,7 +204,7 @@ class MarleyGridInterface(RawGridInterface):
         ## load an arbitrary model file to obtain the wavelength grid
         try:
             base = os.path.expandvars(self.base)
-            wl_file = pd.read_csv(self.base + '/sp_t1000g1000nc_m0.0', names=['wavelength', 'flux'], skiprows=3, delim_whitespace=True)
+            wl_file = pd.read_csv(self.base + '/sp_t1000g1000nc_m0.0', names=['wavelength', 'flux'], skiprows=3, delim_whitespace=True)  # the skiprows=3 should be used for M0 models
         except OSError:
             raise C.GridError("Wavelength file improperly specified.")
         # obtain wavelength
@@ -245,7 +248,7 @@ class MarleyGridInterface(RawGridInterface):
 
         ## load model spectrum
         try:
-            model_file = pd.read_csv(fname, names=['wavelength', 'flux'], skiprows=3, delim_whitespace=True)
+            model_file = pd.read_csv(fname, names=['wavelength', 'flux'], skiprows=3, delim_whitespace=True)  # the skiprows=3 should be used for M0 models
             flux_nu = model_file.flux[::-1].values
         except OSError:
             raise C.GridError("{} is not on disk.".format(fname))
@@ -270,11 +273,11 @@ class MarleyGridInterface(RawGridInterface):
 
 
 class MarleyMp0d5GridInterface(RawGridInterface):
-    ''' The Marley2018 model with super-metallicity of M=+0.5 (positive 0.5 => p0d5)
+    ''' The Marley18 model with super-metallicity of M=+0.5 (positive 0.5 => p0d5)
         '''
     def __init__(self, air=False, norm=False, wl_range=[4000, 50000], base=os.path.expandvars(Starfish.grid["raw_path"])):
         ## basic information of model grids
-        super().__init__(name="Marley",
+        super().__init__(name="MarleyMp0d5",
                          param_names=["temp", "logg"],
                          points=[np.array([200, 225, 250, 300, 325, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2400]),
                                  np.arange(3.5, 5.51, 0.5)],
@@ -290,7 +293,7 @@ class MarleyMp0d5GridInterface(RawGridInterface):
         ## load an arbitrary model file to obtain the wavelength grid
         try:
             base = os.path.expandvars(self.base)
-            wl_file = pd.read_csv(self.base + '/sp_t1000g1000nc_m+0.5', names=['wavelength', 'flux'], skiprows=3, delim_whitespace=True)
+            wl_file = pd.read_csv(self.base + '/sp_t1000g1000nc_m+0.5', names=['wavelength', 'flux'], skiprows=2, delim_whitespace=True)  # the skiprows=2 should be used for M+0.5 models
         except OSError:
             raise C.GridError("Wavelength file improperly specified.")
         # obtain wavelength
@@ -333,7 +336,7 @@ class MarleyMp0d5GridInterface(RawGridInterface):
 
         ## load model spectrum
         try:
-            model_file = pd.read_csv(fname, names=['wavelength', 'flux'], skiprows=3, delim_whitespace=True)
+            model_file = pd.read_csv(fname, names=['wavelength', 'flux'], skiprows=2, delim_whitespace=True)  # the skiprows=2 should be used for M+0.5 models
             flux_nu = model_file.flux[::-1].values
         except OSError:
             raise C.GridError("{} is not on disk.".format(fname))
@@ -355,6 +358,114 @@ class MarleyMp0d5GridInterface(RawGridInterface):
         return (f[self.ind], header)
 # ----
 
+
+
+class MarleygridM0Mp0d5GridInterface(RawGridInterface):
+    ''' The Marley18 model with a metallicity grid of [M0, M+0.5]
+        
+        Note:
+        - the {Teff, logg} grid is adopted as the *overlapped* points between the Marley18_M0 and Marley18_Mp0d5 models
+        - the Z grid is designed as [0, 0.5]
+        - the `base` is not the model path but the one superior to the model path; specific model path would be given based on the metallicity value
+        '''
+    def __init__(self, air=False, norm=False, wl_range=[4000, 50000], base=os.path.expandvars(Starfish.grid["raw_path"])):
+        ## basic information of model grids
+        super().__init__(name="Marley_gridM0Mp0d5",
+                         param_names=["temp", "logg", "Z"],
+                         points=[np.array([200, 225, 250, 300, 325, 350, 375, 400, 425, 450, 475, 500, 525, 550, 575, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2400]),
+                                 np.arange(3.5, 5.51, 0.5),
+                                 np.array([0,0.5])],
+                         air=air,
+                         wl_range=wl_range,
+                         base=base)
+        # norm to decide if normalized to 1 solar luminosity (boolean)
+        self.norm = norm  # deprecated by Gully
+        # dictionary of translating the model parameter (first for Teff and second for log-g)
+        self.par_dicts = [None,
+                          {3.5:"31",4.0:"100",4.5:"316",5.0:"1000",5.5:"3160"},
+                          None]
+
+        ## load an arbitrary model file to obtain the wavelength grid
+        # here I use a Marley18_M0 model spectra
+        try:
+            base = os.path.expandvars(self.base)
+            wl_file = pd.read_csv(self.base + 'Marley18_M0/sp_t1000g1000nc_m0.0', names=['wavelength', 'flux'], skiprows=3, delim_whitespace=True)
+        except OSError:
+            raise C.GridError("Wavelength file improperly specified.")
+        # obtain wavelength
+        wl_full_micron = wl_file.wavelength[::-1].values * u.micron
+        wl_full_Angstrom = wl_full_micron.to(u.Angstrom)
+        # if air is true, convert the normally vacuum file to air wls.
+        if self.air:
+            self.wl_full = vacuum_to_air(wl_full_Angstrom.value)
+        else:
+            self.wl_full = wl_full_Angstrom.value
+
+        ## select wavelength range of interest
+        self.ind = (self.wl_full >= self.wl_range[0]) & (self.wl_full <= self.wl_range[1])
+        self.wl = self.wl_full[self.ind]
+        self.rname = self.base + "{0:}/sp_t{1:0>.0f}g{2:}nc_m{3:{4}.1f}"
+
+    def load_flux(self, parameters, norm=True):
+        '''
+            Load just the flux and header information.
+
+            :param parameters: stellar parameters
+            :type parameters: np.array
+
+            :raises C.GridError: if the file cannot be found on disk.
+
+            :returns: tuple (flux_array, header_dict)
+            '''
+        ## identify the model spectrum based on the chosen parameters
+        # check if parameters are well-defined by the user
+        self.check_params(parameters)
+        # retrieve parameter strings
+        str_parameters = []
+        for param, par_dict in zip(parameters, self.par_dicts):
+            if par_dict is None:
+                str_parameters.append(param)
+            else:
+                str_parameters.append(par_dict[param])
+        # append model name based on the metallicity
+        model_name = ""
+        model_skiprows = 0
+        if parameters[-1]==0.:
+            model_name = "Marley18_M0"
+            model_skiprows = 3
+        elif parameters[-1]==0.5:
+            model_name = "Marley18_Mp0d5"
+            model_skiprows = 2
+        str_parameters = [model_name] + str_parameters
+        # append the string for the sign of metallicity
+        Zsign_str = '+' if parameters[-1] > 0 else ''
+        str_parameters.append(Zsign_str)
+        # model file name
+        fname = self.rname.format(*str_parameters)
+
+        ## load model spectrum
+        try:
+            model_file = pd.read_csv(fname, names=['wavelength', 'flux'], skiprows=model_skiprows, delim_whitespace=True)
+            flux_nu = model_file.flux[::-1].values
+        except OSError:
+            raise C.GridError("{} is not on disk.".format(fname))
+                                                                
+        ## obtain wavelength and flux
+        wl_full_Angstrom = (model_file.wavelength[::-1].values * u.micron).to(u.Angstrom)
+        flux_lambda = (flux_nu * u.erg/u.s/u.cm**2/u.Hz).to(u.erg/u.s/u.cm**2/u.Angstrom, equivalencies=u.spectral_density(wl_full_Angstrom))
+        # check if the normalized spectra are needed, i.e., convert erg/s/cm2/A => erg/s/cm2/A/steradian
+        if self.norm:
+            f = flux_lambda.value / np.pi # convert to erg/cm^2/s/A/steradian
+        else:
+            f = flux_lambda.value
+                                                                                        
+        #Add temp, logg, norm to the metadata
+        header = {}
+        header["norm"] = self.norm
+        header["air"] = self.air
+                                                                                                    
+        return (f[self.ind], header)
+# ----
 
 
 
